09599d2c91e6595d9625fafcd23157d343f9c6bf
alexviera <eveviera244@gmail.com>
2018-12-27 14:23:35 -0600

    Document from 0

---
+++ cuarta5aLimpio.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Ordinario Programaci&oacute;n Web</title>
+	<!-- Vinculación de bulma y Font Awesome -->
+	<style type="text/css">
+		body{margin: 0px;font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;font-size: 11pt;}
+
+		#medio{width: 1024px;margin: 15px auto;}
+		            
+		#migas{width:100%;margin-bottom: 15px;}
+
+	</style>
+	<script type="text/javascript">
+		/*Colocación de código javascript*/
+	</script>
+</head>
+<body>
+<div id="arriba">
+	<p><img src="images/logoUdeC.png" width="300px"></p>
+	<H2 class="leyenda"></H2>
+</div>
+<div id="medio">
+
+	<div id="migas">
+		<p><i class="fa fa-bars"></i>&nbsp;Inicio / Listado</p>
+	</div>
+
+	<div class="campos">
+		<p style='text-align:right;'><a href="formAgrega.php"><i class="fa fa-user"></i>&nbsp;Agregar alumno</a></p>
+		<br/>
+		<!-- Colocación de tabla para listar registros de la base de datos -->
+		
+	</div>
+
+</div>
+
+<div id="abajo">
+	<p><i class='fa fa-copyright derechos'></i></p>
+</div>
+</body>
+</html>
